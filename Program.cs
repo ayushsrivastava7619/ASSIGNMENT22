889670d979a6b814523a71119e78c91a911c2b51
ayushsrivastava7619 <shivamsrivastava23112001@gmail.com>
2023-08-25 09:41:28 +0530

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ASSIGNMENT22
+{
+    static class MathHelper
+    {
+        public static double CalculateAverage(int[] numbers)
+        {
+
+            int sum = 0;
+            foreach (int number in numbers)
+            {
+                sum += number;
+            }
+
+            return (double)sum / numbers.Length;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numberArray = { 10, 20, 30, 40, 50 };
+            double average = MathHelper.CalculateAverage(numberArray);
+
+            Console.WriteLine($"Average: {average:F2}");
+        }
+    }
+}
